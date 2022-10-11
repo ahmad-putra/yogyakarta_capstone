a11a7443683cb0d13a0107a0a1613c7380f2ae70
my name <58381103+ahmad-putra@users.noreply.github.com>
2022-10-12 01:35:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+st.title("MARI KITA LIHAT KESEHATAN DI YOGYAKARTA")
+
+st.markdown('Streamlit apps by [Ahmad Putra](https://github.com/ahmad-putra)')
+
+dataset = pd.read_excel('jenis_penyakit.xlsx')
+dataset
+
+st.caption('(*) Data sementara')
