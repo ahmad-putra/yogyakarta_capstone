e56f21cf9747db91e8d3ac308c180af893274ff1
my name <58381103+ahmad-putra@users.noreply.github.com>
2022-10-12 10:30:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ dataset = pd.read_csv('jenis_penyakit.csv')
 dataset
 
 st.caption('(*) Data sementara')
+
+st.markdown('**Apa makna dari kesehatan ?**\
+    Mengutip dari pernyataan [**Kemkes RI**](https://pusatkrisis.kemkes.go.id/mengenal-makna-kesehatan),\
+    Mengacu pada Undang – Undang nomor 36 tahun 2009 tentang kesehatan,\
+    sehat didefinisikan sebagai suatu keadaan sehat baik secara fisik, mental, spiritual maupun sosial\
+    yang memungkinkan setiap orang untuk hidup produktif secara sosial dan ekonomis.\
+    **Berangkat dari pengertian diatas, dapat diketahui bahwa kesehatan merupakan hal yang luas dan bukan hanya kesehatan secara fisik.**')
